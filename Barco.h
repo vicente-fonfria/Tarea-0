7a87bf7dab435378bb67d5af600aec3b415f3fb7
vicente-fonfria <62406708+vicente-fonfria@users.noreply.github.com>
2020-03-19 21:03:56 -0300

    Barco.h

---
+++ Barco.h
@@ -0,0 +1,12 @@
+#ifndef Barco
+#define Barco
+class Barco
+ {    
+     private:  
+     String nombre;  //field or data member     
+     String id; //field or data member  
+     
+     public:
+     void arribar(float cargaDespacho); 
+ };
+ #endif
